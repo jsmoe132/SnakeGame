f3e9ea2b49f4876145c90ec3d2ca6407e86961ee
jsmoe <jeffreymoe71@gmail.com>
2025-12-16 17:15:16 -0800

    done

---
+++ snake.h
@@ -0,0 +1,22 @@
+#pragma once 
+
+#include <vector>
+using namespace std;
+
+const int WIDTH = 20; 
+const int HEIGHT = 20;
+
+struct Postion {
+    int row;
+    int col;
+};
+
+enum class Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}; 
+
+void runGame(); 
+
